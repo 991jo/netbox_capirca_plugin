b6b9fe3f07b156e5ff314b2ccf3fa863c2c6661d
erwerle <erwerle@belwue.de>
2021-04-20 16:11:59 +0000

    Acl rendering

--- netbox_capirca_plugin/models.py
+++ netbox_capirca_plugin/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.conf import settings
-from django.core.validators import RegexValidator
+from django.core.validators import validate_slug
 from django.urls import reverse
+
 
 from typing import Callable
 from pathlib import Path
 
-from .utils import NamingWrapper, combine_paths_checked
-from .exceptions import UnsupportedTarget, BasePathEscapeError
+from .utils import NamingWrapper, combine_paths_checked, cisco_acl_cleanup
+from .exceptions import UnsupportedTarget
 from capirca.lib.policy import ParsePolicy
 from capirca.lib.cisco import Cisco
 from capirca.lib.ciscoxr import CiscoXR
+from jinja2 import Template
 
-from extras.models import ChangeLoggedModel
+from netbox.models import ChangeLoggedModel
 from dcim.models import Interface
 
-from jinja2 import Template
 
 class ACL(ChangeLoggedModel):
 
@@ -27,7 +28,7 @@ class ACL(ChangeLoggedModel):
     static_definitions_dir = models.TextField()
     policy_template_path = models.TextField()
 
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, validators=[validate_slug])
 
     description = models.CharField(max_length=255, blank=True)
 
@@ -72,9 +73,9 @@ class ACL(ChangeLoggedModel):
 
         try:
             base_dir = settings.PLUGINS_CONFIG["netbox_capirca_plugin"]["policy_base_path"]
-            policy = ParsePolicy(policy_text, naming, base_dir=base_dir)
+            ParsePolicy(policy_text, naming, base_dir=base_dir)
         except Exception as e:
-            errors["terms"]= f"Error while parsing policy: { e }"
+            errors["terms"] = f"Error while parsing policy: { e }"
             raise ValidationError(errors)
 
     def get_full_definitions_dir(self) -> str:
@@ -103,8 +104,7 @@ class ACL(ChangeLoggedModel):
 
         return combine_paths_checked(base_path, Path(self.policy_template_path))
 
-
-    def render(self, target=None) -> str:
+    def render(self, target: str = None, options: str = "") -> str:
         """
         Renders the ACL.
 
@@ -118,9 +118,9 @@ class ACL(ChangeLoggedModel):
         with open(self.get_full_policy_template_path()) as f:
             template_text = f.read()
         template = Template(template_text)
-        policy_text = template.render(acl=self)
+        policy_text = template.render(acl=self, target=target, options=options)
 
-        base_dir= settings.PLUGINS_CONFIG["netbox_capirca_plugin"]["policy_base_path"]
+        base_dir = settings.PLUGINS_CONFIG["netbox_capirca_plugin"]["policy_base_path"]
         policy = ParsePolicy(policy_text, naming, base_dir=base_dir)
 
         render_function = self.get_render_function(target)
@@ -133,10 +133,10 @@ class ACL(ChangeLoggedModel):
         return render_function
 
     def _render_cisco(self, policy) -> str:
-        return Cisco(policy, 1)
+        return cisco_acl_cleanup(str(Cisco(policy, 1)))
 
     def _render_ciscoxr(self, policy) -> str:
-        return CiscoXR(policy, 1)
+        return cisco_acl_cleanup(str(CiscoXR(policy, 1)))
 
     def get_absolute_url(self):
         return reverse("plugins:netbox_capirca_plugin:acl",
@@ -145,8 +145,17 @@ class ACL(ChangeLoggedModel):
     def __str__(self):
         return f"ACL { self.name }"
 
+
 class ACLInterfaceAssignment(ChangeLoggedModel):
 
     interface = models.OneToOneField(Interface, on_delete=models.CASCADE)
-    ingress = models.ForeignKey(ACL, on_delete=models.PROTECT, related_name="%(class)s_ingress", blank=True, null=True)
-    egress = models.ForeignKey(ACL, on_delete=models.PROTECT, related_name="%(class)s_egress", blank=True, null=True)
+    ingress = models.ForeignKey(ACL,
+                                on_delete=models.PROTECT,
+                                related_name="%(class)s_ingress",
+                                blank=True,
+                                null=True)
+    egress = models.ForeignKey(ACL,
+                               on_delete=models.PROTECT,
+                               related_name="%(class)s_egress",
+                               blank=True,
+                               null=True)
